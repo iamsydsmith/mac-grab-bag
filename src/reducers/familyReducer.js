1dfd0f1fdc3d3358e1eec504cd9737a3f054554f
sydneysmith <sydney.smith@turner.com>
2018-11-02 10:50:46 -0400

    redux working, receiving data

---
+++ src/reducers/familyReducer.js
@@ -0,0 +1,32 @@
+const defaultState = {
+  family: []
+};
+
+function familyReducer(state = defaultState, action) {
+  switch (action.type) {
+    case "DATA_RECEIVE": {
+      return Object.assign({}, state, {
+        family: action.payload.data
+      });
+    }
+
+    case "DATA_REQUEST": {
+      return Object.assign({}, state, {
+        isLoading: true
+      });
+    }
+
+    case "DATA_RECEIVE_INVALID": {
+      return Object.assign({}, state, {
+        lastChecked: Date.now(),
+        isLoading: false
+      });
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
+
+module.exports = familyReducer;
